09c610573a505579ad4d315521cb32a300563a05
wiut00006931 <suktamov2@students.wiut.uz>
2020-10-30 14:29:09 +0500

    My First commit

---
+++ DSCC_CW1_6931_FRONTEND/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSCC_CW1_6931_FRONTEND.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
